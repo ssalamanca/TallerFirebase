4bf8f3fe42b71e1a8711069a7e6c5550db1ab2e0
Andres Contreras <andres_c8@outlook.com>
2018-11-01 15:42:32 -0500

    cambios

--- app/src/main/java/com/example/santi/tallerfirebase/DatabaseActivity.java
+++ app/src/main/java/com/example/santi/tallerfirebase/DatabaseActivity.java
@@ -3,11 +3,19 @@ package com.example.santi.tallerfirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DatabaseActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+
+
     }
 }
